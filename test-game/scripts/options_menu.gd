68c0b5a5a4d9aa7372a9295954cab03bafac7464
Fernando Hirsch <fernandobhirsch@gmail.com>
2024-06-24 21:30:38 -0300

    Make fullscreen system

--- test-game/scripts/options_menu.gd
+++ test-game/scripts/options_menu.gd
@@ -6,8 +6,8 @@ signal back_button_pressed
 var music_bus = AudioServer.get_bus_index("Music")
 var sound_effect_bus = AudioServer.get_bus_index("Sound Effects")
 
-@onready var music_slider = $VBoxContainer/MusicSlider
-@onready var sound_effect_slider = $VBoxContainer/SoundEffectSlider
+@onready var music_slider = %MusicSlider
+@onready var sound_effect_slider = %SoundEffectSlider
 
 
 func _ready():
@@ -34,3 +34,15 @@ func slider_value_changed(bus, value):
 		AudioServer.set_bus_mute(bus, true)
 	else:
 		AudioServer.set_bus_mute(bus, false)
+
+
+func _on_resolution_button_item_selected(index):
+	if index == 0:
+		DisplayServer.window_set_size(DisplayServer.screen_get_size())
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN)
+	elif index == 1:
+		DisplayServer.window_set_size(DisplayServer.screen_get_size())
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+	else:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+		DisplayServer.window_set_size(Vector2i(1280, 720))
